051fd14b1717f58d6f76f393c2ae806b6bf27b54
rosariofe <felipeoliver@hotmail.com>
2020-03-03 12:09:55 -0300

    Calculadora concluido

--- Calculadora.java
+++ Calculadora.java
@@ -1,19 +1,55 @@
 import javax.swing.JOptionPane;
 
 public class Calculadora{
-
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
     
-        int a;
-        int b;
-        int num;
+        double a;
+        double b;
+        double aux;
+        double result;
 
-        a = Integer.parseInt((JOptionPane.showInputDialog("Digite o primeiro numero")));
-        b =Integer.parseInt((JOptionPane.showInputDialog("Digite o segundo numero ")));
+        aux =   Double.parseDouble((JOptionPane.showInputDialog(null, "Digite a operacao: \n 1 -Soma \n 2 -Subtracao \n 3- Multiplicacao \n 4- Divisao \n 0- Sair ")))  ;
 
-        num = a-b;
+        if (aux == 1){
+        
+            a = Double.parseDouble((JOptionPane.showInputDialog("Digite o primeiro numero")));
+            b = Double.parseDouble((JOptionPane.showInputDialog("Digite o segundo numero ")));
 
-        JOptionPane.showMessageDialog(null,"O resultado"+num);
+            result = a+b;
 
-    }
+            JOptionPane.showMessageDialog(null,"O resultado da soma"+result);
+        }
+        
+        if(aux ==2){
+
+            a = Double.parseDouble((JOptionPane.showInputDialog("Digite o primeiro numero")));
+            b = Double.parseDouble((JOptionPane.showInputDialog("Digite o segundo numero")));
+
+            result = a -b ;
+
+            JOptionPane.showMessageDialog(null, "O resultado da subtracao " +result);
+
+        }
+
+        if (aux == 3){
+
+            a = Double.parseDouble((JOptionPane.showInputDialog("Digite o primeiro numero")));
+            b = Double.parseDouble((JOptionPane.showInputDialog("Digite o segundo numero")));
+
+            result = a * b ;
+
+            JOptionPane.showMessageDialog(null,"O resultado da multiplicacao" +result);
+        }
+        
+        if (aux == 4){
+           
+            a = Double.parseDouble((JOptionPane.showInputDialog("Digite o primeiro numero")));
+            b = Double.parseDouble((JOptionPane.showInputDialog("Digite o segundo numero")));
+
+
+            result = a / b ;
+
+            JOptionPane.showMessageDialog(null, "O resultado da divisao" +result);
+        }
+    }   
 }
