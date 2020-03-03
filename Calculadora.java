3e4459959160f031dee609f7a94014cca6be9f71
rosariofe <felipeoliver@hotmail.com>
2020-03-02 21:29:32 -0300

    Version First

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Calculadora{
+
+    public static void main(String[] args) {
+    
+        int a;
+        int b;
+        int num;
+
+        a = Integer.parseInt((JOptionPane.showInputDialog("Digite o primeiro numero")));
+        b =Integer.parseInt((JOptionPane.showInputDialog("Digite o segundo numero ")));
+
+        num = a+b;
+
+        JOptionPane.showMessageDialog(null,"O resultado"+num);
+
+    }
+}
